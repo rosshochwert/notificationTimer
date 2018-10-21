b1786df0c7ed8dec9ef7ee3af2851867707e89ed
Ross Hochwert <rhochwert@gmail.com>
2018-10-21 15:57:59 -0500

    added notifications

--- notificationTimer.js
+++ notificationTimer.js
@@ -14,18 +14,91 @@ function checkNotifications(){
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	checkNotifications();
+	//checkNotifications();
 });
 
-function notifyMe() {
+function notifyMe(input) {
   if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    setTimeout(function(){ 
-    	var notification = new Notification("Hi there!");
-    }, 3000);
+    if (input=="example"){
+    	var notification = new Notification({
+    		title: "This is what your notification will look like!",
+    		body:"Test",
+    		img: ""
+    	});
+  	}
   }
 
   // Finally, if the user has denied notifications and you 
   // want to be respectful there is no need to bother them any more.
 }
 
+var timerInterval;
+
+function setTimer(){
+	notifyMe("example");
+	duration1 = moment.duration({
+		'seconds': 0,
+		'hour': 0,
+		'minutes': 0,
+		'days':0
+	});
+
+	duration2 = moment.duration({
+	    'seconds': document.getElementById("inputSeconds").value,
+	    'hour': document.getElementById("inputHours").value,
+	    'minutes': document.getElementById("inputMinutes").value,
+	    'days':0
+	});
+
+	diff=duration2-duration1;
+	duration=moment.duration(diff, 'milliseconds');
+
+	timesLeft = document.getElementById("inputRepeat").value;
+
+	document.getElementById("timesLeft").innerHTML = "# of recurrences left: " + timesLeft;
+
+	interval = 1000;
+	timerInterval = setInterval(function(){
+		duration = moment.duration(duration.asMilliseconds() - interval, 'milliseconds');
+		if (duration < 0){
+			timesLeft = timesLeft - 1;
+			if (timesLeft <= 0){
+				document.getElementById("seconds").innerHTML = "0s";
+				document.getElementById("minutes").innerHTML = "0m";
+				document.getElementById("hours").innerHTML = "0h";
+				document.getElementById("timesLeft").innerHTML = "# of recurrences left: 0";	
+			} else {
+				duration2 = moment.duration({
+	    			'seconds': document.getElementById("inputSeconds").value,
+	    			'hour': document.getElementById("inputHours").value,
+	    			'minutes': document.getElementById("inputMinutes").value,
+	    			'days':0
+				});
+					diff=duration2-duration1;
+					duration=moment.duration(diff, 'milliseconds');
+					document.getElementById("timesLeft").innerHTML = "# of recurrences left: " + timesLeft;
+					//notifyMe();
+
+			}
+		} else {
+			document.getElementById("seconds").innerHTML = duration.seconds() + "s";
+			document.getElementById("minutes").innerHTML = duration.minutes() + "m";
+			document.getElementById("hours").innerHTML = duration.hours() + "h";
+		}
+	}, interval);
+
+	document.getElementById("timer").style.display = 'block';
+	document.getElementById("setTimerButton").style.display = 'none';
+}
+
+function stopTimer(){
+	console.log("stopping timer");
+	clearInterval(timerInterval);
+	document.getElementById("setTimerButton").style.display = 'block';
+	document.getElementById("timer").style.display = 'none';
+	document.getElementById("seconds").innerHTML = "s";
+	document.getElementById("minutes").innerHTML = "m";
+	document.getElementById("hours").innerHTML = "h";
+	document.getElementById("timesLeft").innerHTML = "";
+}
