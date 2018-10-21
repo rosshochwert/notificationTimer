ac71711dd94b8542a3aced4f5e6a38600d9578ed
Ross Hochwert <rhochwert@gmail.com>
2018-10-21 14:10:56 -0500

    slight change

--- notificationTimer.js
+++ notificationTimer.js
@@ -18,28 +18,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function notifyMe() {
-  // Let's check if the browser supports notifications
-  console.log(Notification.permission);
-  
-  if (!("Notification" in window)) {
-    alert("This browser does not support system notifications");
-  }
-
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === "granted") {
+  if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
-    console.log("notification");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
-    Notification.requestPermission(function (permission) {
-      // If the user accepts, let's create a notification
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
-    });
+    setTimeout(function(){ 
+    	var notification = new Notification("Hi there!");
+    }, 3000);
   }
 
   // Finally, if the user has denied notifications and you 
